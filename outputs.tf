6caad6c44de68aa0983c308e2516dda0cb4a404f
Patrick Raco <pataraco@gmail.com>
2020-05-24 16:25:51 -0700

    fix splat notation

--- outputs.tf
+++ outputs.tf
@@ -12,14 +12,14 @@ output "iam_role_name" {
 
 output "instance_profile_arn" {
   description = "The ARN of the instance profile created"
-  value       = var.create_profile ? aws_iam_instance_profile.this[0].arn : null
+  value       = var.create_profile ? aws_iam_instance_profile.this[*].arn : null
   # value = aws_iam_instance_profile.this.arn
   # value = join("", aws_iam_instance_profile.this.*.arn)
 }
 
 output "instance_profile_name" {
   description = "The name of the instance profile created"
-  value       = var.create_profile ? aws_iam_instance_profile.this[0].name : null
+  value       = var.create_profile ? aws_iam_instance_profile.this[*].name : null
   # value = aws_iam_instance_profile.this.name
   # value = join("", aws_iam_instance_profile.this.*.name)
 }
